919b9abcb196da95efa99286803ba3f69f7535b1
Karen Ascencio <ascencio.karen@yahoo.com.mx>
2021-08-14 19:47:59 -0500

    added user usecase

--- src/usecases/users.js
+++ src/usecases/users.js
@@ -3,3 +3,30 @@ const User = require('../models/users')
 function create(data){
     return User.create(data)
 }
+
+function getAll(){
+    return User.find()
+}
+
+function getById(id){
+    return User.find({_id: id})
+}
+
+function updateById(id, newData) { 
+    return User.findByIdAndUpdate(id, newData, {new: true})
+}
+
+function remove(id){
+    return User.findByIdAndDelete(id)
+}
+
+module.exports = {
+    create,
+    getAll,
+    getById,
+    updateById,
+    remove
+}
+
+
+
