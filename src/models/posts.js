9583a8c155cd50108c08456ef38572b379b2053c
Karen Ascencio <ascencio.karen@yahoo.com.mx>
2021-08-18 15:04:29 -0500

    some changes

--- src/models/posts.js
+++ src/models/posts.js
@@ -31,14 +31,14 @@ const postSchema = new mongoose.Schema({
         min:0,
     },
     published_at:{
-        type:String,
+        type:Date,
         required:true,
-        match:/\d{4}-\d{2}-\d{2}.{1}\d{2}:\d{2}:\d{2}.\d{3}.{1}/g
+        // match:/\d{4}-\d{2}-\d{2}.{1}\d{2}:\d{2}:\d{2}.\d{3}.{1}/g
     },
     published_timestamp:{
-        type:String,
+        type:Date,
         required:true,
-        match:/\d{4}-\d{2}-\d{2}.{1}\d{2}:\d{2}:\d{2}.\d{3}.{1}/g
+        // match:/\d{4}-\d{2}-\d{2}.{1}\d{2}:\d{2}:\d{2}.\d{3}.{1}/g
     },
     readable_publish_date:{
         type:String,
