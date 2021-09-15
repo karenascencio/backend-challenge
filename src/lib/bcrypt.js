7ca7575a1877432fcbaf97089c212503e15676f5
freddy Castuera <freddycastuera@outlook.es>
2021-09-15 00:29:39 -0600

    agregamos la credenciales

--- src/lib/bcrypt.js
+++
@@ -1,9 +0,0 @@
-// Requiring the library bcrypt
-const bcrypt = require('bcrypt')
-function hash(plainText){
-    return bcrypt.hash(plainText, 10)
-}
-module.exports = {
-    ...bcrypt,
-    hash
-}
