bc5a931c457225edc995c574d74e696daae49ca6
freddy Castuera <freddycastuera@outlook.es>
2021-09-20 14:21:21 -0600

    montamos el bucket

--- src/routers/posts.js
+++ src/routers/posts.js
@@ -3,6 +3,36 @@ const router = express.Router()
 const Posts = require('../usecases/posts')
 const verification = require ('../middlewares/authMiddle') 
 
+
+//librerias para subir la imagen a bucket
+const multer = require("multer");
+const path = require('path')
+const AWS = require("aws-sdk");
+const imageStorage = multer.diskStorage({
+  // Destination to store image     
+  destination: 'images', 
+    filename: (req, file, cb) => {
+        cb(null, file.fieldname + '_' + Date.now() 
+           + path.extname(file.originalname))
+          // file.fieldname is name of the field (image)
+          // path.extname get the uploaded file extension
+  }
+});
+const imageUpload = multer({
+  storage: imageStorage,
+  limits: {
+    fileSize: 1000000 // 1000000 Bytes = 1 MB
+  },
+  fileFilter(req, file, cb) {
+    if (!file.originalname.match(/\.(png|jpg)$/)) { 
+       // upload only png and jpg format
+       return cb(new Error('Please upload a Image'))
+     }
+   cb(undefined, true)
+}
+}) 
+
+
 //router.use(verification)
 router.get('/', async (request, response) => {
     try{
@@ -43,8 +73,53 @@ router.get('/:id', async (request, response) => {
         })
     }
 })
-router.post('/', async (request,response)=>{
+router.post('/',imageUpload.single('image'),async (request,response)=>{
     try{
+
+        //monta esto en la ruta de posts imageUpload.single('image')
+//todo esto va dentro de la ruta de poste de post
+AWS.config.getCredentials(function(err) {
+    if (err) console.log(err.stack);
+    // credentials not loaded
+    else {
+      console.log("Access key:", AWS.config.credentials.accessKeyId);
+    }
+  });
+  AWS.config.update({region: 'us-east-2'});
+  console.log("Region: ", AWS.config.region);
+  
+  // Create S3 service object
+  s3 = new AWS.S3({apiVersion: '2006-03-01'});
+  
+  // call S3 to retrieve upload file to specified bucket
+  var uploadParams = {Bucket:'image-dev-to', Key: '', Body: ''};
+  var file = request.file.path;
+  
+  // Configure the file stream and obtain the upload parameters
+  var fs = require('fs');
+  var fileStream = fs.createReadStream(file);
+  fileStream.on('error', function(err) {
+    console.log('File Error', err);
+  });
+  uploadParams.Body = fileStream;
+  var path = require('path');
+  uploadParams.Key = path.basename(file);
+  
+  // call S3 to retrieve upload file to specified bucket
+  s3.upload (uploadParams, function (err, data) {
+    if (err) {
+      console.log("Error", err);
+    } if (data) {
+        //aqui tenemos acceso a la url donde gaurdamos nuestra imagen en el bucket
+      console.log("Upload Success", data.Location);
+      request.body.cover_image=data.location
+    }
+  });
+
+
+
+
+
         const post = await Posts.createPost(request.body);
         response.json({
             success: true,
